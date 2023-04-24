4d7e3c73bdcb53cdaad4583bb7ea64750009dd5f
juancar347 <juancar347@gmail.com>
2023-04-24 19:00:06 +0200

    ejemplos_abril_python

--- abril_24.4.py
+++ abril_24.4.py
@@ -6,3 +6,15 @@ def sumariva(price):
     return price * 1.21
 print(f"El resultado es: {list(map(sumariva, prices))}")
 
+
+
+class Persona:
+    def__init__(this, first_name, last_name):
+        this.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        
+        name = "Mi nombre"
+        person1 = person('person1', 'apellido1')
+        
+   
